d30d102463e7218a4ee4f3ad563f4dffbe4137c0
Rahul Ballal <rballal@csod.com>
2015-09-16 15:45:49 -0700

    Initial Commit

---
+++ templify.ps1
@@ -0,0 +1,23 @@
+$targetFolder=$args[0]
+
+Write-Host "Setting up repository structure for " $targetFolder
+
+# Constants
+$vsGitIgnore=".gitignore"
+$readme="README.md"
+
+
+# Script
+Copy-Item $vsGitIgnore -Destination $targetFolder
+New-Item $targetFolder/README.md -type file
+New-Item $targetFolder/drop-solution-file-here.txt -type file
+
+
+mkdir $targetFolder/src
+New-Item $targetFolder/src/drop-your-csproj-folders-here.txt -type file
+
+mkdir $targetFolder/build_automation
+New-Item $targetFolder/build_automation/drop-your-build-scripts-here.txt -type file
+
+mkdir $targetFolder/tools
+New-Item $targetFolder/tools/drop-tools-like-xunitrunner-here.txt -type file
