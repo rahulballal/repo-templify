12c5eda6272817ec9cf67cd69cc914eed6e93e61
Rahul Ballal <rballal@csod.com>
2015-11-16 17:54:51 -0800

    adds octopack support

--- repo/build_automation/ps-modules/msbuild.psm1
+++ repo/build_automation/ps-modules/msbuild.psm1
@@ -29,4 +29,38 @@ function Invoke-MsBuild
     }
 }
 
+function Invoke-MsBuildWithOctopack
+{
+  [CmdletBinding()]
+  Param
+  (
+      [String]
+      [ValidateScript({Test-Path $_ })]
+      $solutionPath,
+
+      [String]
+      [ValidateScript({Test-Path $_ })]
+      $outputDirectoryPath,
+
+      [String]
+      [ValidateSet("Build","Clean","Rebuild")]
+      $target = "Rebuild",
+
+      [String]
+      [ValidateSet("quiet","minimal","normal","detailed","diagnostic")]
+      $verbosity = "minimal",
+
+      [String]
+      [ValidateSet("Debug","Release")]
+      $buildConfig = "Release",
+
+      [String]
+      $octopackProperties =""
+  )
+
+  Exec {
+      msbuild $solutionPath /t:$target /v:$verbosity /p:Configuration=$buildConfig /p:OutDir=$outputDirectoryPath /p:RunOctoPack=true $octopackProperties
+  }
+}
+
 #Export-ModuleMember -Function Invoke-MsBuild
